74d28d82d468d07546d5a5609e133d7d2d1b4327
Metin Ilgar <ilgarmutlu@outlook.com.tr>
2022-02-22 15:05:42 +0300

    Version 0.2 Update

--- encryption.py
+++ encryption.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def ascii2hex(ascii_val):
     return ascii_val.encode().hex()
 
@@ -70,3 +72,81 @@ def decodingImg(Image,long):
         elif long == z:
             break
     return "".join(decode_list)
+
+def encryptImg2(text, Image):
+    l1= []
+    l2= []
+    height = Image.shape[0]
+    width = Image.shape[1]
+
+    res = ''.join(format(i, '08b') for i in bytearray(text, encoding='utf-8'))
+    if len(res) % 3 == 0:
+        pass
+    else:
+        for x in range(0, (3 - (len(res) % 3))):
+            res = res + "0"
+
+    for h in range(0,height,2):
+        for w in range(0,width):
+            l1.append(Image[h,w])
+
+            if len(res) <= len(l1)*3:
+                break
+        if len(res) <= len(l1)*3:
+           break
+
+    k = 0
+    for a1,a2,a3 in l1:
+        l2.append(np.array([a1+int(res[k]), a2+int(res[k+1]), a3+int(res[k+2])], dtype=np.uint8))
+        k = k + 3
+
+    for h in range(1,height,2):
+        for w in range(0,width):
+            Image[h,w] = l2[w]
+
+            if len(l2)-1 == w:
+                break
+        if len(l2) - 1 == w:
+            break
+    return Image
+
+def decodingImg2(Image):
+    height = Image.shape[0]
+    width = Image.shape[1]
+    binary = ""
+    count = 0
+    count2 = 0
+    for h in range(0, height, 2):
+        for w in range(0, width):
+            a = (Image[h+1, w] - Image[h, w])
+            for i in a:
+                binary = binary + str(i)
+                if i == 0:
+                    count2 = count2 + 1
+
+                    if count2 == 3:
+                        count = count + 1
+
+            count2 = 0
+
+            if count == 100:
+                break
+        if count == 100:
+            break
+    b = ""
+    bl = []
+    for x in binary:
+        b = b+x
+        if len(b) % 8 == 0:
+            if int(b) == 0:
+                b = ""
+                continue
+            bl.append(b)
+            b=""
+
+    return str(bytes([int(x,2) for x in bl]).decode('utf-8'))
+
+
+
+
+
--- run.py
+++ run.py
@@ -3,15 +3,30 @@ import cv2 as cv
 import sys
 import argparse
 
+# Project Information
 parser = argparse.ArgumentParser(prog = "Text-in-picture encryption.",
                                  description = "This application is for encryption and decryption on the picture.",
                                  epilog = "This software was created by Metin Ilgar Mutlu.")
-
-parser.add_argument("--encode", "-enc", help = """Indicates that it wants to encrypt the text into the image.\n
-                                               Example: python run.py -enc -img picture.png -txt 'sample text'""",
+#------------------------------------ Commands for Version 0.1 ------------------------------------
+# Encryption version 0.1
+parser.add_argument("--encode", "-enc",
+                    help = """Indicates that it wants to encrypt the text into the image.\n
+                    Example: python run.py -enc -img picture.png -txt 'sample text'""",
                     action="store_true")
-parser.add_argument("--decode", "-dec", help = """Indicates that you want to decrypt the image.\n
-                                               Example: python run.py -dec -img picture.png -len 100""",
+# Decryption version 0.1
+parser.add_argument("--decode", "-dec",
+                    help = """Indicates that you want to decrypt the image.\n
+                    Example: python run.py -dec -img picture.png -len 100""",
+                    action="store_true")
+#------------------------------------ Commands for Version 0.2 ------------------------------------
+# Encryption version 0.2
+parser.add_argument("--encode2", "-enc2",
+                    help = """Indicates that it wants to encrypt the text into the image.\n
+                     Example: python run.py -enc2 -img picture.png -txt 'sample text'""",
+                    action="store_true")
+# Decryption version 0.2
+parser.add_argument("--decode2", "-dec2", help = """Indicates that you want to decrypt the image.\n
+                                               Example: python run.py -dec2 -img picture.png""",
                     action="store_true")
 parser.add_argument("--image", "-img" , help = "Specifies the file path of the image.", type=str, required=True)
 parser.add_argument("--text", "-txt", help = "Specifies the text you want to encrypt.", type=str)
@@ -23,15 +38,24 @@ if img is None:
     sys.exit("Could not read the image.")
 
 
-if data.encode == True and data.decode == False:
+if data.decode == False and data.encode == True and data.decode2 == False and data.encode2 == False:
     encryption.encryptImg(data.text,img)
     cv.imwrite("EncryptionImage.png", img)
-elif data.decode == True and data.encode == False:
+elif data.decode == True and data.encode == False and data.decode2 == False and data.encode2 == False:
     text = encryption.decodingImg(img,data.length)
     output = open("output.txt", "w")
     output.write(text)
     output.close()
     print(text)
+elif data.decode == False and data.encode == False and data.decode2 == True and data.encode2 == False:
+    text = encryption.decodingImg2(img)
+    output = open("output.txt", "w")
+    output.write(text)
+    output.close()
+    print(text)
+elif data.decode == False and data.encode == False and data.decode2 == False and data.encode2 == True:
+    encryption.encryptImg2(data.text, img)
+    cv.imwrite("EncryptionImage.png", img)
 else:
     print("Wrong use.")
 
